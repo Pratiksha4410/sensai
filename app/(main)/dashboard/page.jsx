1e3ed9d017eebe78e28df702f95a0a42678ec4c6
Amiya <vaishnavimahajan713@gmail.com>
2025-06-22 18:29:28 +0530

    preparing for deployment

---
+++ app/(main)/dashboard/page.jsx
@@ -0,0 +1,24 @@
+import { getIndustryInsights } from "@/actions/dashboad";
+import { getUserOnboardingStatus } from "@/actions/user";
+import { redirect } from "next/navigation";
+import React from "react";
+import DashboardView from "./_components/dashboard-view";
+
+const IndustryInsightsPage = async () => {
+  const { isOnboarded } = await getUserOnboardingStatus();
+
+  // If not onboarded, redirect to onboarding page
+  // Skip this check if already on the onboarding page
+  if (!isOnboarded) {
+    redirect("/onboarding");
+  }
+  const insights = await getIndustryInsights();
+
+  return (
+    <div className="container mx-auto">
+      <DashboardView insights={insights} />
+    </div>
+  );
+};
+
+export default IndustryInsightsPage;
